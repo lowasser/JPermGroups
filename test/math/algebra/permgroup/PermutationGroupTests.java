f664e99e66fdb805de911a7cdd4a1939403bdfdf
Louis Wasserman <wasserman.louis@gmail.com>
2011-05-18 16:53:14 -0500

    Added some tests for extensions and for direct products

--- test/math/algebra/permgroup/PermutationGroupTests.java
+++ test/math/algebra/permgroup/PermutationGroupTests.java
@@ -1,7 +1,6 @@
 package math.algebra.permgroup;
 
 import com.google.common.base.Predicate;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
@@ -11,35 +10,43 @@ import math.algebra.permgroups.permutation.Permutations;
 
 public class PermutationGroupTests extends TestCase {
   private final ImmutableSet<Integer> domainP = ImmutableSet.of(1, 2, 3, 4);
-  private final ImmutableSet<Integer> domainQ = ImmutableSet.of(1, 2, 3);
+  private final ImmutableSet<Integer> domainQ = ImmutableSet.of(5, 6, 7);
+  private final ImmutableSet<Integer> domainPQ = ImmutableSet
+    .<Integer> builder().addAll(domainP).addAll(domainQ).build();
+
   private final Permutation<Integer> p1 = Permutations.permutation(ImmutableMap
     .of(1, 2, 2, 1, 3, 3, 4, 4));
   private final Permutation<Integer> p2 = Permutations.permutation(ImmutableMap
     .of(1, 2, 2, 3, 3, 1, 4, 4));
   private final Permutation<Integer> p3 = Permutations.permutation(ImmutableMap
     .of(1, 3, 3, 1, 2, 2, 4, 4));
+  private final Permutation<Integer> q1 = Permutations.permutation(ImmutableMap
+    .of(5, 7, 7, 5, 6, 6));
+  private final Permutation<Integer> pq1 = Permutations
+    .permutation(ImmutableMap.<Integer, Integer> builder().putAll(p2.asMap())
+      .putAll(q1.asMap()).build());
 
-  public void testCyclicGroup1() {
-    PermutationGroup<Integer> group1 = PermutationGroup.generateGroup(domainP,
-        p1);
+  @SuppressWarnings("unchecked") public void testCyclicGroup1() {
+    PermutationGroup<Integer> group1 =
+        PermutationGroup.generateGroup(domainP, p1);
     assertEquals(2, group1.size());
     assertTrue(group1.contains(Permutations.identity(domainP)));
     assertTrue(group1.contains(p1));
     assertFalse(group1.contains(p2));
   }
 
-  public void testCyclicGroup2() {
-    PermutationGroup<Integer> group2 = PermutationGroup.generateGroup(domainP,
-        p2);
+  @SuppressWarnings("unchecked") public void testCyclicGroup2() {
+    PermutationGroup<Integer> group2 =
+        PermutationGroup.generateGroup(domainP, p2);
     assertEquals(3, group2.size());
     assertTrue(group2.contains(Permutations.identity(domainP)));
     assertFalse(group2.contains(p1));
     assertTrue(group2.contains(p2));
   }
 
-  public void testGroup12() {
-    PermutationGroup<Integer> group12 = PermutationGroup.generateGroup(domainP,
-        p1, p2);
+  @SuppressWarnings("unchecked") public void testGroup12() {
+    PermutationGroup<Integer> group12 =
+        PermutationGroup.generateGroup(domainP, p1, p2);
     assertEquals(6, group12.size());
     assertTrue(group12.contains(Permutations.identity(domainP)));
     assertTrue(group12.contains(p1));
@@ -47,13 +54,13 @@ public class PermutationGroupTests extends TestCase {
     assertTrue(group12.contains(p3));
   }
 
-  public void testSubgroup() {
-    PermutationGroup<Integer> group1 = PermutationGroup.generateGroup(domainP,
-        p1);
-    PermutationGroup<Integer> group2 = PermutationGroup.generateGroup(domainP,
-        p2);
-    PermutationGroup<Integer> group12 = PermutationGroup.generateGroup(domainP,
-        p1, p2);
+  @SuppressWarnings("unchecked") public void testSubgroup() {
+    PermutationGroup<Integer> group1 =
+        PermutationGroup.generateGroup(domainP, p1);
+    PermutationGroup<Integer> group2 =
+        PermutationGroup.generateGroup(domainP, p2);
+    PermutationGroup<Integer> group12 =
+        PermutationGroup.generateGroup(domainP, p1, p2);
     assertTrue(group1.isSubgroupOf(group12));
     assertTrue(group2.isSubgroupOf(group12));
     assertTrue(group12.isSubgroupOf(group12));
@@ -62,17 +69,25 @@ public class PermutationGroupTests extends TestCase {
     assertFalse(group12.isSubgroupOf(group1));
   }
 
-  public void testSubgroupPredicate() {
-    PermutationGroup<Integer> group1 = PermutationGroup.generateGroup(domainP,
-        p1);
-    PermutationGroup<Integer> group12 = PermutationGroup.generateGroup(domainP,
-        p1, p2);
-    Predicate<Permutation<Integer>> stabilizes3 = new Predicate<Permutation<Integer>>() {
+  @SuppressWarnings("unchecked") public void testSubgroupPredicate() {
+    PermutationGroup<Integer> group1 =
+        PermutationGroup.generateGroup(domainP, p1);
+    PermutationGroup<Integer> group12 =
+        PermutationGroup.generateGroup(domainP, p1, p2);
+    Predicate<Permutation<Integer>> stabilizes3 =
+        new Predicate<Permutation<Integer>>() {
 
-      @Override public boolean apply(Permutation<Integer> input) {
-        return Permutations.stabilizes(input, 3);
-      }
-    };
+          @Override public boolean apply(Permutation<Integer> input) {
+            return Permutations.stabilizes(input, 3);
+          }
+        };
     assertEquals(group1, group12.subgroup(stabilizes3));
+  }
+
+  @SuppressWarnings("unchecked") public void testDirectProduct() {
+    PermutationGroup<Integer> g = PermutationGroup.generateGroup(domainP, p2);
+    PermutationGroup<Integer> h = PermutationGroup.generateGroup(domainQ, q1);
+    PermutationGroup<Integer> gh = PermutationGroup.directProduct(g, h);
+    assertEquals(gh, PermutationGroup.generateGroup(domainPQ, pq1));
   }
 }
--- test/math/algebra/permgroup/permutation/PermutationTests.java
+++ test/math/algebra/permgroup/permutation/PermutationTests.java
@@ -2,6 +2,11 @@ package math.algebra.permgroup.permutation;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 
 import junit.framework.TestCase;
 import math.algebra.permgroups.permutation.Permutation;
@@ -9,13 +14,15 @@ import math.algebra.permgroups.permutation.Permutations;
 
 public class PermutationTests extends TestCase {
   private final ImmutableSet<Integer> domainP = ImmutableSet.of(1, 2, 3, 4);
-  private final ImmutableSet<Integer> domainQ = ImmutableSet.of(1, 2, 3);
+  private final ImmutableSet<Integer> domainQ = ImmutableSet.of(5, 6, 7);
   private final Permutation<Integer> p1 = Permutations.permutation(ImmutableMap
     .of(1, 2, 2, 1, 3, 3, 4, 4));
   private final Permutation<Integer> p2 = Permutations.permutation(ImmutableMap
     .of(1, 2, 2, 3, 3, 1, 4, 4));
   private final Permutation<Integer> p3 = Permutations.permutation(ImmutableMap
     .of(1, 3, 3, 1, 2, 2, 4, 4));
+  private final Permutation<Integer> q1 = Permutations.permutation(ImmutableMap
+    .of(5, 7, 7, 5, 6, 6));
 
   public void testComposition() {
     testComposition(p1, p2);
@@ -47,5 +54,24 @@ public class PermutationTests extends TestCase {
       assertEquals(q.image(p.image(i)), pq.image(i));
     }
     assertEquals(pq.domain(), p.domain());
+  }
+
+  @SuppressWarnings("unchecked") public void testExtensions() {
+    ImmutableSet<Integer> domainPQ =
+        ImmutableSet.<Integer> builder().addAll(domainP).addAll(domainQ)
+          .build();
+    for (Permutation<Integer> p : Arrays.asList(p1, p2, p3, q1,
+        p1.extend(domainPQ).compose(q1.extend(domainPQ)))) {
+      testExtension(p, domainPQ);
+    }
+  }
+
+  public void testExtension(Permutation<Integer> p, Set<Integer> domain) {
+    Map<Integer, Integer> perm = Maps.newHashMap();
+    for (Integer i : domain) {
+      perm.put(i, i);
+    }
+    perm.putAll(p.asMap());
+    assertEquals(Permutations.permutation(perm), p.extend(domain));
   }
 }
