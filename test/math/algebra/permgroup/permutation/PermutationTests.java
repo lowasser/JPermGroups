49424b7dfb6f65d937bde6c0e616cf06a1960d7c
Louis Wasserman <wasserman.louis@gmail.com>
2011-05-23 15:51:15 -0500

    Fixed up PermutationTests

--- test/math/algebra/permgroup/permutation/PermutationTests.java
+++ test/math/algebra/permgroup/permutation/PermutationTests.java
@@ -1,12 +1,10 @@
 package math.algebra.permgroup.permutation;
 
+import static math.structures.permutation.Permutations.compose;
+
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Maps;
-
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Set;
+import com.google.common.collect.Iterables;
 
 import junit.framework.TestCase;
 import math.structures.permutation.Permutation;
@@ -29,49 +27,28 @@ public class PermutationTests extends TestCase {
     testComposition(p2, p1);
   }
 
+  public void testComposition(Permutation<Integer> p, Permutation<Integer> q) {
+    Permutation<Integer> pq = compose(p, q);
+    for (Integer i : Iterables.concat(p.support(), q.support())) {
+      assertEquals(p.apply(q.apply(i)), pq.apply(i));
+    }
+  }
+
   public void testEquals() {
-    assertEquals(p3, p1.compose(p2));
-    assertFalse(p3.equals(p2.compose(p1)));
+    assertEquals(p3, compose(p2, p1));
+    assertFalse(p3.equals(compose(p1, p2)));
   }
 
   public void testEqualsIdentity() {
-    Permutation<Integer> idP = Permutations.identity(domainP);
-    assertFalse(idP.equals(Permutations.identity(domainQ)));
-    assertTrue(idP.equals(Permutations.identity(domainP)));
+    Permutation<Integer> idP = Permutations.identity();
+    assertFalse(idP.equals(Permutations.identity()));
+    assertTrue(idP.equals(Permutations.identity()));
   }
 
   public void testInverse() {
-    Permutation<Integer> idP = Permutations.identity(domainP);
-    assertEquals(idP, p1.compose(p1.inverse()));
-    assertEquals(idP, p1.inverse().compose(p1));
-    assertEquals(idP, p1.compose(p1.inverse()));
+    Permutation<Integer> idP = Permutations.identity();
+    assertEquals(idP, compose(p1,p1.inverse()));
+    assertEquals(idP, compose(p1.inverse(),p1));
     assertFalse(idP.equals(p1));
-  }
-
-  public void testComposition(Permutation<Integer> p, Permutation<Integer> q) {
-    Permutation<Integer> pq = p.compose(q);
-    for (Integer i : p.domain()) {
-      assertEquals(q.image(p.image(i)), pq.image(i));
-    }
-    assertEquals(pq.domain(), p.domain());
-  }
-
-  @SuppressWarnings("unchecked") public void testExtensions() {
-    ImmutableSet<Integer> domainPQ =
-        ImmutableSet.<Integer> builder().addAll(domainP).addAll(domainQ)
-          .build();
-    for (Permutation<Integer> p : Arrays.asList(p1, p2, p3, q1,
-        p1.extend(domainPQ).compose(q1.extend(domainPQ)))) {
-      testExtension(p, domainPQ);
-    }
-  }
-
-  public void testExtension(Permutation<Integer> p, Set<Integer> domain) {
-    Map<Integer, Integer> perm = Maps.newHashMap();
-    for (Integer i : domain) {
-      perm.put(i, i);
-    }
-    perm.putAll(p.asMap());
-    assertEquals(Permutations.permutation(perm), p.extend(domain));
   }
 }
